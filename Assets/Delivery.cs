6b238ce75f3cc2a32745dd2bdd6780bf9a44c78a
Tin-Nguyen-Student <ting566@student.oaisd.org>
2023-03-07 10:01:55 -0500

    Commit and push, TIN!

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] float destroyDelay = 0.5F;
+    bool hasPackage;
+    
+    private void Start()
+    {
+
+    }
     public void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("You Belong To The Streets!");
@@ -13,10 +20,13 @@ public class Delivery : MonoBehaviour
         if (other.tag == "Package")
         {
             Debug.Log("Package picked up");
+            hasPackage = true;
+            Destroy(other.gameObject, destroyDelay);
         }
-        if (other.tag == "Customer")
+        if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package Delivered");
+            hasPackage = false;
         }
     }
 }
